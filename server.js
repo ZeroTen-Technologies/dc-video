7fd6727dad85268b964b84bfd85b3edd4f22b656
ome chukwuemeka <omeco4christ@gmail.com>
2022-02-19 12:23:36 +0000

    2nd commit

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const server = require("http").Server(app);
+const { v4:uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
  
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .send('Hello server is running')
+//     .end();
+// });
+
+app.get('/',(req,rsp)=> {
+  rsp.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req,rsp)=> {
+  rsp.render("room",{roomId: req.params.room});
 });
  
 // Start the server
